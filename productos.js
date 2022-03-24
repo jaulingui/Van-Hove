5023e66837c758d5c89211e87cca662fb4c0bee6
jaulingui <92231987+jaulingui@users.noreply.github.com>
2022-03-24 12:28:40 -0300

    Pagina al 24-03

---
+++ productos.js
@@ -0,0 +1,9 @@
+function ocultar(elemento){
+    var caja = document.getElementById(elemento)
+    caja.style.display = `none`
+}
+
+function mostrar (elemento){
+    var caja = document.getElementById(elemento)
+    caja.style.display = `block`
+}
